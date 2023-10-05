36325b32bb6e71ad61c1aa6523dab25a210a3eb5
Erick Hilda Andreas <erickh.andreas@gmail.com>
2023-10-05 11:29:11 +0700

    add initial ui for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,33 @@
 import { Suspense } from "react";
 import Carousel from "@/components/ui/carousel";
 import Navbar from "@/components/ui/navbar";
+import { BASE_API_URL, MOCK_API_URL } from "@/lib/env";
 
 async function getData() {
-  const res = await fetch("http://localhost:8080/muflix/listmovie");
+  const res = await fetch(`${MOCK_API_URL}/muflix/listmovie`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
   const data = await res.json();
-  return data.slice(0, 15);
+  const series = data.filter((item: any) => item.Type === "series");
+  const movies = data.filter((item: any) => item.Type === "movie");
+  return { series, movies };
 }
 
 export default async function Home() {
   const data = await getData();
 
+  if (data !== null && Object.keys(data).length > 0) {
+    console.log(data);
+  }
+
   return (
     <main className="h-full w-full">
       <Navbar />
       <Suspense fallback={<div>Loading...</div>}>
-        <Carousel items={data} title="Popular TV show" />
+        <Carousel items={data.movies} title="Popular Movies" />
+        <Carousel items={data.series} title="Popular Series" />
       </Suspense>
     </main>
   );
